dcc465d1639756e904734e7112b1960ad9659718
Samuel-Thuku <samuelngigithuku@gmail.com>
2023-04-25 20:11:12 +0000

    Task 0 correction

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
 int Format(const char *format, va_list args);
 int invalidSpec(char previousFormat, char format, int num1);
 int validate(char type);
 int printOut(char c);
+#endif
