4c38a255a38cb6a3dcac313b83b5a0ba770a8ad1
Samuel-Thuku <samuelngigithuku@gmail.com>
2023-04-30 07:41:53 +0000

    Corrections

--- 1_functions.c
+++ 1_functions.c
@@ -1,15 +1,5 @@
 #include "main.h"
 /**
-*_putchar - Writes a character to stdout
-*@c: The character to write
-*Return: On success, 1. On error, -1.
-**/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
 *print_char - Print a character
 *@args: va_list argument containing the character to be printed
 *Return: Number of characters printed (always 1)
