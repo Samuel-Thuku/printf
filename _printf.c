c3620552e0ad407b17c88671e972366b3045b046
Samuel-Thuku <samuelngigithuku@gmail.com>
2023-04-30 07:38:56 +0000

    Corrections

--- _printf.c
+++ _printf.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+*struct specifier - struct specifiers
+*@c: the conversion specifier
+*@handler: the function pointer
+**/
+
+typedef struct specifier
+{
+	char c;
+	int (*handler)(va_list args);
+} specifier_t;
+specifier_t specifiers[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_int},
+		{'i', print_int},
+		{'\0', NULL}
+	};
+/**
  * _printf - a function that produces output according to a format.
  * @format: is a character string.
  * Return: number of characters printed.
